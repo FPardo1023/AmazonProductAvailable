5cfc516202250c8d6dc466853eaaba6e3c5131c1
Fabio <fpardo1023@gmail.com>
2020-06-13 00:56:12 -0400

    Checking something out

--- amazon_script.py
+++ amazon_script.py
@@ -14,7 +14,6 @@ def check_availability(URL_py, user_agent_py):
     product = soup.find(id="productTitle").get_text()
     availability = soup.find(id="availability").get_text()
 
-
     print(product.strip())
     if "Currently unavailable." in availability:
         print("Currently Unavailable!")
@@ -22,3 +21,4 @@ def check_availability(URL_py, user_agent_py):
         print(availability.strip())
 
 check_availability('https://www.amazon.ca/dp/B07YGZL8XF/?coliid=I18V4B7R8TULPQ&colid=3CBNXXUKOYLM5&ref_=lv_ov_lig_dp_it&th=1', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
+
